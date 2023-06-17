b940a56b941a17c2c23e6e2e22e6133d30843978
Aranclanos <Aranclanos@gmail.com>
2023-06-17 19:28:58 -0300

    Adds ear fluff to the cylinders

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -3,27 +3,31 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Character : MonoBehaviour
+
+namespace Characters
 {
-	private NavMeshAgent navMeshAgent;
-
-	public int dignity;
-
-	public Vector3 DEBUGtargetDestination;
-
-	private void Awake()
+	public class Character : MonoBehaviour
 	{
-		navMeshAgent = GetComponent<NavMeshAgent>();
-	}
+		private NavMeshAgent navMeshAgent;
 
-	private void Update()
-	{
-		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+		public int dignity;
+
+		public Vector3 DEBUGtargetDestination;
+
+		private void Awake()
 		{
-			navMeshAgent.destination = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
-			DEBUGtargetDestination = navMeshAgent.destination;
+			navMeshAgent = GetComponent<NavMeshAgent>();
 		}
+
+		private void Update()
+		{
+			if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+			{
+				navMeshAgent.destination = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+				DEBUGtargetDestination = navMeshAgent.destination;
+			}
+		}
+
 	}
-
-
 }
+
