444571151eb157211a2713f29e5595046f10de65
Aranclanos <Aranclanos@gmail.com>
2023-06-17 18:04:21 -0300

    Adds the administration room class

---
+++ Assets/Scripts/Rooms/Administration.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Administration : MonoBehaviour
+{
+
+	public void OpenTradeRoute()
+	{
+		
+	}
+
+	public void CollectTaxes()
+	{
+		
+	}
+}
