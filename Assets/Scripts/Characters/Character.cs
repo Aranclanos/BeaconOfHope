7861d6a7303aa3c43a2cb0816d42f837b460d809
Aranclanos <Aranclanos@gmail.com>
2023-06-17 18:58:15 -0300

    Adds random movement with navmesh. Character names will now face the camera.

--- Assets/Scripts/Characters/Character.cs
+++ Assets/Scripts/Characters/Character.cs
@@ -1,18 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Character : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+	private NavMeshAgent navMeshAgent;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public int dignity;
+
+	public Vector3 DEBUGtargetDestination;
+
+	private void Awake()
+	{
+		navMeshAgent = GetComponent<NavMeshAgent>();
+	}
+
+	private void Update()
+	{
+		if (navMeshAgent.remainingDistance <= navMeshAgent.stoppingDistance)
+		{
+			navMeshAgent.destination = new Vector3(Random.Range(-20, 20), 0, Random.Range(-20, 20));
+			DEBUGtargetDestination = navMeshAgent.destination;
+		}
+	}
+
+
 }
